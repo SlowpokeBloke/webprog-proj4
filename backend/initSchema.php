a23e626d6135b8602e042daac64113c155ee4983
mcoca <mcoca4280@gmail.com>
2023-12-09 18:24:37 -0500

    sql query for each table

--- backend/initSchema.php
+++ backend/initSchema.php
@@ -13,6 +13,21 @@ if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "CREATE TABLE IF NOT EXISTS user (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    passwrd VARCHAR(255) NOT NULL
+    )";
+if ($conn->query($sql) === TRUE) {
+    echo "Tables created successfully";
+}
+else {
+    echo "Error creating table: " . $conn->error;
+}
+
 /** create properties table if not exist
  */
 $sql = "CREATE TABLE IF NOT EXISTS property (
@@ -28,16 +43,28 @@ $sql = "CREATE TABLE IF NOT EXISTS property (
     parking INT,
     price DECIMAL,
     )";
+if ($conn->query($sql) === TRUE) {
+    echo "Tables created successfully";
+}
+else {
+    echo "Error creating table: " . $conn->error;
+}
 
 $sql = "CREATE TABLE IF NOT EXISTS addr (
-        id INT NOT NULL,
-        prop_id INT NOT NULL,
-        street VARCHAR NOT NULL.
-        city VARCHAR NOT NULL,
-        state VARCHAR NOT NULL,
-        zip INT NOT NULL,
-        FOREIGN KEY (prop_id) REFERENCES property(id)
+    id INT NOT NULL,
+    prop_id INT NOT NULL,
+    street VARCHAR NOT NULL.
+    city VARCHAR NOT NULL,
+    state VARCHAR NOT NULL,
+    zip INT NOT NULL,
+    FOREIGN KEY (prop_id) REFERENCES property(id)
     )";
+if ($conn->query($sql) === TRUE) {
+    echo "Tables created successfully";
+}
+else {
+    echo "Error creating table: " . $conn->error;
+}
 
 $sql = "CREATE TABLE IF NOT EXISTS img (
     id INT NOT NULL,
@@ -45,12 +72,18 @@ $sql = "CREATE TABLE IF NOT EXISTS img (
     img_name VARCHAR NOT NULL,
     img_type VARCHAR NOT NULL,
     FOREIGN KEY (prop_id) REFERENCES property(id)
-)";
+    )";
+if ($conn->query($sql) === TRUE) {
+    echo "Tables created successfully";
+}
+else {
+    echo "Error creating table: " . $conn->error;
+}
 
 $sql = "CREATE TABLE IF NOT EXISTS owner (
     user_id INT NOT NULL,
     prop_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
     FOREIGN KEY (prop_id) REFERENCES property(id)
 )";
 

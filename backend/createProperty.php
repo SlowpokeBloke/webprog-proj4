1677c1a9415aca5c9f3c8ad18c510be9dec098de
mcoca <mcoca4280@gmail.com>
2023-12-09 17:51:55 -0500

    php to create property tables in db cleared null in frontend

---
+++ backend/createProperty.php
@@ -0,0 +1,70 @@
+<?php
+$host = "localhost";
+$user = "mcoca1";
+$pass = "mcoca1";
+$dbname = "mcoca1";
+
+//Create connection
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+//Check connection
+if($conn->connect_error) {
+    echo "Could not connect to server\n";
+    die("Connection failed: " . $conn->connect_error);
+}
+
+/** create properties table if not exist
+ */
+$sql = "CREATE TABLE IF NOT EXISTS property (
+    id INT NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    prop_description VARCHAR,
+    sqft INT(6),
+    acreage DECIMAL,
+    rooms INT(6),
+    baths DECIMAL,
+    bedrms INT(6),
+    year INT(6),
+    yard BOOLEAN,
+    parking INT,
+    price DECIMAL,
+    )";
+
+$sql = "CREATE TABLE IF NOT EXISTS addr (
+        id INT NOT NULL,
+        prop_id INT NOT NULL,
+        street VARCHAR NOT NULL.
+        city VARCHAR NOT NULL,
+        state VARCHAR NOT NULL,
+        zip INT NOT NULL,
+        FOREIGN KEY (prop_id) REFERENCES property(id)
+    )";
+
+$sql = "CREATE TABLE IF NOT EXISTS img (
+    id INT NOT NULL,
+    prop_id INT NOT NULL,
+    img_name VARCHAR NOT NULL,
+    img_type VARCHAR NOT NULL,
+    FOREIGN KEY (prop_id) REFERENCES property(id)
+)";
+
+$sql = "CREATE TABLE IF NOT EXISTS owner (
+    user_id INT NOT NULL,
+    prop_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (prop_id) REFERENCES property(id)
+)";
+
+// Inserting to the table
+// $sql = "INSERT INTO property (firstname)
+// VALUES ('$prop_address', '$prop_description')";
+
+//Confirmation message
+if ($conn->query($sql) === TRUE) {
+    echo "Tables created successfully";
+}
+else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>

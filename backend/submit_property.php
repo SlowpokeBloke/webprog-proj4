a829e03a01cbf7065fca1743f51df8f52a3d0d95
mcoca <mcoca4280@gmail.com>
2023-12-10 18:27:39 -0500

    switched db connection method

--- backend/submit_property.php
+++ backend/submit_property.php
@@ -3,15 +3,41 @@
 // $user = "dosgood1";
 // $pass = "dosgood1";
 // $dbname = "dosgood1";
- 
+
+//Create connection
+$db = mysqli_connect($host, $user, $pass, $dbname);
+    
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+  }
+$stmt = $conn->prepare("INSERT INTO property (detail, sqft, acreage, rooms, baths, bedrms, yr, yard, parking, price)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("sidiibid", $detail, $sqft, $baths, $bedrooms, $year, $yard, $parking, $price);
+// $stmt = $conn->prepare("INSERT INTO property (detail, sqft, acreage, rooms, baths, bedrms, yr, yard, parking, price)
+// VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+// $stmt->bind_param("sididiibid", $detail, $sqft, $acreage, $rooms, $baths, $bedrooms, $year, $yard, $parking, $price);
+
+$detail = $_POST["facilities"];
+$sqft = $_POST["floor-plan"];
+//$acreage = $_POST[""];
+//$rooms = $_POST[""];
+$baths = $_POST["baths"];
+$bedrooms = $_POST["bedrooms"];
+$year = $_POST["age"];
+$yard = $_POST["garden"];
+$parking = $_POST["parking"];
+$price = $_POST["property-value"];
+$stmt->execute();
+
+echo "Property data created successfully";
+$stmt->close();
+
 // If upload button is clicked ...
-if (isset($_POST['submit'])) {
- 
     $filename = $_FILES["uploadfile"]["name"];
     $tempname = $_FILES["uploadfile"]["tmp_name"];
     $folder = "../images/" . $filename;
- 
-    $db = mysqli_connect($host, $user, $pass, $dbname);
  
     // Get all the submitted data from the form
     $sql = "INSERT INTO img (img_name, img_type) VALUES ('$filename', '?')";
@@ -25,5 +51,7 @@ if (isset($_POST['submit'])) {
     } else {
         echo "<h3>  Failed to upload image!</h3>";
     }
-}
+$conn->close();
+header("Location: ../frontend/seller.html");
+exit();
 ?>

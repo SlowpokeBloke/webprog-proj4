12b9e2dd963416e032653300a8ec29e807564483
mcoca <mcoca4280@gmail.com>
2023-12-10 20:14:39 -0500

    saves result from table select and echoes json

--- backend/fetch_property.php
+++ backend/fetch_property.php
@@ -11,12 +11,9 @@ if($conn->connect_error) {
 $id = $_SESSION["user_id"];
 $sql = "SELECT property.id, property.detail, property.sqft, property.baths, property.bedrms, property.yr, property.yard, property.parking, property.price
 FROM property WHERE owner = '$id'";
-$data = array(
-    'name' => 'John',
-    'age' => 30,
-    'city' => 'New York'
-);
+$result = mysqli_query($conn, $sql);
 
-$json = json_encode($data);
+$rows = mysqli_fetch_all($result);
+$json = json_encode($rows);
 echo "<script>var data = $json;</script>";
 ?>

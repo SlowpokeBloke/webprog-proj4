ab325eb067dbdaeb03786ddc788eca1b521ee0ec
mcoca <54874341+SlowpokeBloke@users.noreply.github.com>
2023-12-10 20:29:35 -0500

    Update propScript.js

--- frontend/propScript.js
+++ frontend/propScript.js
@@ -93,12 +93,8 @@ function closePropertyModal() {
 }
 //Simulate fetching property data from the server (replace with actual fetch logic)
 function fetchPropertiesFromServer() {
-    // Fetch properties from the server using a fetch API or another method
-    // properties = [ ... ]; // Update properties with fetched data
-    // Then, call generatePropertyCards to display the cards
     generatePropertyCards();
 
 }
 
-//Initialize property cards by fetching data from the server
 fetchPropertiesFromServer();
